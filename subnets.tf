a4402c1442f58fdd95e49006808e29ad2be35779
MohammedAbdulFarhan08 <mohdfarhantech08@gmail.com>
2026-09-23 23:42:43 +0530

    Add public and private subnets in subnets.tf

---
+++ subnets.tf
@@ -0,0 +1,20 @@
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "subnet-public-${local.name_suffix}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = var.availability_zone
+
+  tags = {
+    Name = "subnet-private-${local.name_suffix}"
+  }
+}
