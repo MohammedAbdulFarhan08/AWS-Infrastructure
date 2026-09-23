e1571f089b2e1d91965bdded8effc9b340bc5303
MohammedAbdulFarhan08 <mohdfarhantech08@gmail.com>
2026-09-23 23:43:15 +0530

    Add public and private route tables with associations

---
+++ routing.tf
@@ -0,0 +1,37 @@
+# Public route table -> Internet Gateway
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "rt-public-${local.name_suffix}"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Private route table -> NAT Gateway
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.id
+  }
+
+  tags = {
+    Name = "rt-private-${local.name_suffix}"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
